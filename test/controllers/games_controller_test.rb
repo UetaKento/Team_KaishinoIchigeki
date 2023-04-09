e9b357e739a9038924852475804ac3abd1f1766a
skrmch5 <skrmch5@gmail.com>
2023-04-09 14:06:24 +0900

    ページ遷移実装

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,8 +1,18 @@
 require "test_helper"
 
 class GamesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get games_index_url
+  test "should get top" do
+    get games_top_url
+    assert_response :success
+  end
+
+  test "should get play" do
+    get games_play_url
+    assert_response :success
+  end
+
+  test "should get result" do
+    get games_result_url
     assert_response :success
   end
 end
